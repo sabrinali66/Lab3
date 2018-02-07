f14904d55cc3ed1b9953a0d451a88067e52a9464
sabrinali66 <sabrinali.66@gmail.com>
2018-02-07 14:39:30 -0600

    New Changes.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Here");
         // write your code here
+    }
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
 }
